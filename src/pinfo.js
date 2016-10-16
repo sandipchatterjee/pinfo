078304cbb0e19bf9afc2000ade869580a1ee351b
Sandip Chatterjee <sandip.chatterjee@gmail.com>
2016-10-16 16:41:53 -0700

    Working histogram plots based on D3 examples
    Filled with randomly-generated data for now

--- src/pinfo.js
+++ src/pinfo.js
@@ -102,3 +102,66 @@ function makeImageHists(imageData) {
     console.log("blues: "+blues.length);
     console.log(blues);
 }
+
+var pinfoBox = d3.select('#photo-info-box');
+var MAXPLOTWIDTH = getMaxImageViewWidth(pinfoBox)
+
+function makeHist(data, element, label="new histogram") {
+
+    var formatCount = d3.format(",.0f");
+
+    var margin = {top: 10, right: 30, bottom: 30, left: 30},
+        width = MAXPLOTWIDTH - margin.left - margin.right,
+        height = Math.floor(MAXPLOTWIDTH/2) - margin.top - margin.bottom;
+
+    var x = d3.scaleLinear()
+        .rangeRound([0, width]);
+
+    var bins = d3.histogram()
+        .domain(x.domain())
+        .thresholds(x.ticks(30))
+        (data);
+
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(bins, function(d) { return d.length; })])
+        .range([height, 0]);
+
+    element.append("p")
+           .text(label);
+
+    var svg = element.append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var bar = svg.selectAll(".bar")
+        .data(bins)
+      .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+    bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(bins[0].x1) - x(bins[0].x0) - 5)
+        .attr("height", function(d) { return height - y(d.length); });
+
+    svg.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    element.transition()
+           .duration(1200)
+           .style("opacity", 1);
+
+}
+
+var data_reds = d3.range(500000).map(d3.randomNormal(0.2,0.4));
+makeHist(data_reds, d3.select("#photo-info-red"), "reds");
+
+var data_greens = d3.range(500000).map(d3.randomNormal(0.6,0.1));
+makeHist(data_greens, d3.select("#photo-info-green"), "greens");
+
+var data_blues = d3.range(500000).map(d3.randomBates(10));
+makeHist(data_blues, d3.select("#photo-info-blue"), "blues");
